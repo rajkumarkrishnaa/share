21a879dd96a22c95808a2eb6b98a79cbf94b08c8
rajkumarkrishnaa <rajkumar237@gmail.com>
2021-04-29 15:15:09 +0530

    new one

--- src/org/combine/Class1.java
+++ src/org/combine/Class1.java
@@ -2,4 +2,17 @@ package org.combine;
 
 public class Class1 {
 
+	public void myName() {
+	System.out.println("Rajkumar");
+	}
+	public void myNumber() {
+	System.out.println("123456789");
+	}
+	public void myNative() {
+	System.out.println("chennai");
+	}
+	public static void main(String[] args) {
+		Class1 a = new Class1();
+		a.myName();a.myNumber();a.myNative();
+	}
 }
